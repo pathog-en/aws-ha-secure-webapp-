2d288fa783ee443b979008d17d879f1530eeb1b0
Pat Hogen <patrick.hogen@gmail.com>
2026-01-03 16:49:38 -0600

    Add Terraform variables for subnet configurations

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "availability_zones" {
+  description = "Availability Zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_app_subnet_cidrs" {
+  description = "CIDR blocks for private application subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "private_db_subnet_cidrs" {
+  description = "CIDR blocks for private database subnets"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24"]
+}
