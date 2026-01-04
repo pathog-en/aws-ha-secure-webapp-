67a8426521d7124835a86dd1018a221c5d1fdabe
Pat Hogen <patrick.hogen@gmail.com>
2026-01-04 17:09:46 -0600

    Remove unused Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,3 @@
-variable "availability_zones" {
-  description = "Availability Zones to use"
-  type        = list(string)
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-}
-
-variable "private_app_subnet_cidrs" {
-  description = "CIDR blocks for private application subnets"
-  type        = list(string)
-}
-
-variable "private_db_subnet_cidrs" {
-  description = "CIDR blocks for private database subnets"
-  type        = list(string)
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
 
 
 
