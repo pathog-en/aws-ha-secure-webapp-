7e1d6ae24d0845c2acc2163401b1c5fc2040b880
Pat Hogen <patrick.hogen@gmail.com>
2026-01-04 13:28:14 -0600

    Refactor VPC and subnet resource definitions

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,4 +38,14 @@ resource "aws_subnet" "private_db" {
     Name = "private-db-subnet-${count.index + 1}"
   }
 }
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "ha-secure-webapp-vpc"
+  }
+}
+
 
