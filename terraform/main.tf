807dd60fe2fd63596e139d5587ac59b66b4041aa
Pat Hogen <patrick.hogen@gmail.com>
2026-01-04 13:53:20 -0600

    Replace VPC resource with new configuration
    Removed existing VPC resource and provider configuration. Added new VPC resource with updated CIDR block and tags.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+resource "aws_vpc" "this" {
+  cidr_block = "10.50.0.0/16"
+
+  tags = {
+    Name = "terraform-test-vpc"
   }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_vpc" "test" {
-  cidr_block = "10.99.0.0/16"
 }
 
 
