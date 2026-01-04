d6200a9697e7de345a7fd90f5e0b5b4ef38b88b1
Pat Hogen <patrick.hogen@gmail.com>
2026-01-04 14:10:50 -0600

    Add public and private subnets to VPC configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,38 @@ resource "aws_vpc" "this" {
     Name = "ha-secure-webapp-vpc"
   }
 }
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+resource "aws_subnet" "private_app" {
+  count             = length(var.private_app_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_app_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "private-app-subnet-${count.index + 1}"
+  }
+}
+resource "aws_subnet" "private_db" {
+  count             = length(var.private_db_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_db_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "private-db-subnet-${count.index + 1}"
+  }
+}
+
 
 
 
